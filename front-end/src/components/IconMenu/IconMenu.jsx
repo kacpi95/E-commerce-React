69656dd7aa65be0f0d0e2045150e30dabe5d23b1
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-19 19:08:48 +0200

    add TopBar component with styles

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import BAG_ICON from '../../assets/bag.svg';
 import HEART from '../../assets/heart.svg';
-import style from './IconMenu.module.css';
+import styles from './IconMenu.module.css';
 
 export function IconMenu() {
   const cartItems = 2;
   return (
     <div>
-      <ul className={style.iconMenu}>
+      <ul className={styles.iconMenu}>
         <li>
           <Link to='/ulubione'>
             <img src={HEART} alt='heart-icon' />
@@ -16,7 +16,7 @@ export function IconMenu() {
         <li>
           <Link to='/koszyk'>
             <img src={BAG_ICON} alt='bag-icon' />
-            <div className={style.numberOfProducts}>{cartItems}</div>
+            <div className={styles.numberOfProducts}>{cartItems}</div>
           </Link>
         </li>
       </ul>
--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -3,14 +3,20 @@ import { MainMenu } from '../MainMenu/MainMenu';
 import { Logo } from '../Logo/Logo';
 import { CurrencySelector } from '../CurrencySelector/CurrencySelector';
 import { IconMenu } from '../IconMenu/IconMenu';
+import { TopBar } from '../TopBar/TopBar';
 
 export function Layout() {
   return (
     <>
-      <MainMenu />
-      <Logo />
-      <CurrencySelector />
-      <IconMenu />
+      <TopBar>
+        <MainMenu />
+        <Logo />
+        <div>
+          <CurrencySelector />
+          <IconMenu />
+        </div>
+      </TopBar>
+
       <Footer />
     </>
   );
