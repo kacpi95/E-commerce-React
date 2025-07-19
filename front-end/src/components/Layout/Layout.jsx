1b63a7a7a5fcfa68d6cd3f24cb0d902c6b01adae
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-19 16:17:04 +0200

    add Logo component with styles

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Footer } from '../Footer/Footer';
 import { MainMenu } from '../MainMenu/MainMenu';
+import { Logo } from '../Logo/Logo';
 
 export function Layout() {
   return (
     <>
       <MainMenu />
+      <Logo />
       <Footer />
     </>
   );
