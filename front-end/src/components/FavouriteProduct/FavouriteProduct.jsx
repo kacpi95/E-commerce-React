101430de67999a4be83b661772456772aafa672b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-22 11:56:50 +0200

    fix FavouriteProduct component and add styles

--- front-end/src/components/FavouriteProduct/FavouriteProduct.jsx
+++ front-end/src/components/FavouriteProduct/FavouriteProduct.jsx
@@ -1,21 +1,23 @@
 import REMOVE_IMG from '../../assets/remove.svg';
 import BAG_ICON from '../../assets/bag.svg';
+import styles from './FavouriteProduct.module.css';
 
 export function FavouriteProduct({ product }) {
   return (
-    <div>
+    <div className={styles.favouriteProduct}>
       <img src={`${product.photos[0]}`} />
-      <div>
-        <div>
+      <div className={styles.favouriteProductInfo}>
+        <div className={styles.topRow}>
           <h3>
-            {product.brnad} {product.productName}
+            {product.brand} {product.productName}
           </h3>
-          <p>
-            <span>Cena: </span>
-            {product.price}zł
-          </p>
+          <p>{product.pricePLN}zł</p>
         </div>
-        <div>
+        <p className={styles.priceRow}>
+          <span>Cena: </span>
+          {product.pricePLN}zł
+        </p>
+        <div className={styles.buttonRow}>
           <button>
             <img src={REMOVE_IMG} />
             Usuń
