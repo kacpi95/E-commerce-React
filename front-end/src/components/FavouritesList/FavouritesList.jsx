7c506c0cf6a0705c287ba0426c03105069d2bf2f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-22 11:44:21 +0200

    add styles to FavouritesList component

--- front-end/src/components/FavouritesList/FavouritesList.jsx
+++ front-end/src/components/FavouritesList/FavouritesList.jsx
@@ -1,10 +1,11 @@
 import { CenteredContent } from '../CenteredContent/CenteredContent';
 import { FavouriteProduct } from '../FavouriteProduct/FavouriteProduct';
+import styles from './FavouritesList.module.css';
 
 export function FavouritesList({ products }) {
   return (
     <CenteredContent>
-      <div>
+      <div className={styles.favouritesList}>
         <h2>Ulubione</h2>
         <div>
           {products.map((product) => {
