f7f217db0539aa0ce66b6deaaed888e20117ab29
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-22 12:41:28 +0200

    update FlexContainer component

--- front-end/src/components/FlexContainer/FlexContainer.jsx
+++ front-end/src/components/FlexContainer/FlexContainer.jsx
@@ -1,5 +1,18 @@
 import { CartProductList } from '../CartProductsList/CartProductsList';
+import { CenteredContent } from '../CenteredContent/CenteredContent';
+import styles from './FlexContainer.module.css';
 
-export function FlexContainer() {
-  return 
+export function FlexContainer({ products }) {
+  return (
+    <CenteredContent>
+      <div className={styles.favouritesList}>
+        <h2>Ulubione</h2>
+        <div>
+          {products.map((product) => {
+            return <CartProductList key={product.id} product={product} />;
+          })}
+        </div>
+      </div>
+    </CenteredContent>
+  );
 }
