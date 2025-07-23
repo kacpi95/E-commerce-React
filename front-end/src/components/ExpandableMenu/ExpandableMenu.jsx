624d07744d26d8af9ad8a270211fcd8a364e76fe
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-23 21:35:29 +0200

    add styles to ExpandableMenu

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,15 +1,19 @@
 import { NavLink } from 'react-router-dom';
 import { CATEGORIES } from '../../constants/category';
+import AROW_ICON from '../../assets/arrow.svg';
+import styles from './ExpandableMenu.module.css';
 
 export function ExpandableMenu() {
   return (
-    <div>
+    <div className={styles.expandableMenu}>
       <p>Kobieta</p>
       <ul>
         {CATEGORIES.map((category) => {
           return (
             <li key={category.path}>
-              <NavLink to={category.path}>{category.categoryName}</NavLink>
+              <NavLink to={category.path}>
+                {category.categoryName} <img src={AROW_ICON} />
+              </NavLink>
               <ul>
                 {category.subcategories.map((subcategory) => {
                   return (
