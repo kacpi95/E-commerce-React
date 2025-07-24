e4f36a1ed162edee085453bf7c1fd573ac9c8122
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-24 21:53:58 +0200

    add state and active styles to componen Accordion

--- front-end/src/components/Accordion/Accordion.jsx
+++ front-end/src/components/Accordion/Accordion.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import ARROW_ICON from '../../assets/arrow.svg';
 import styles from './Accordion.module.css';
 
 export function Accordion({ items }) {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
   return (
     <ul>
-      {items.map((item) => {
+      {items.map((item, index) => {
         return (
-          <li key={item.title}>
+          <li
+            key={item.title}
+            onClick={() => {
+              setActiveItemIndex(index);
+            }}
+          >
             <div className={styles.item}>
               <p>{item.title}</p>
-              <img src={ARROW_ICON} />
+              <img
+                className={activeItemIndex === index ? styles.expanded : ''}
+                src={ARROW_ICON}
+              />
             </div>
-            <p>{item.content}</p>
+            {activeItemIndex === index && <p>{item.content}</p>}
           </li>
         );
       })}
