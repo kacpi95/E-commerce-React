86d8f851ad4301b260302030a5c9e22d779648f4
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-24 20:56:12 +0200

    add state view photo and class active in Photos component

--- front-end/src/components/Photos/Photos.jsx
+++ front-end/src/components/Photos/Photos.jsx
@@ -1,15 +1,26 @@
 import styles from './Photos.module.css';
 import { FlexContainer } from '../FlexContainer/FlexContainer';
+import { useState } from 'react';
 
 export function Photos({ product }) {
+  const [currentPhoto, setCurrentPhoto] = useState(product.photos[0]);
   return (
     <FlexContainer>
       <div className={styles.thumbnails}>
         {product.photos.map((photo) => {
-          return <img key={photo} src={photo} />;
+          return (
+            <img
+              className={currentPhoto === photo ? styles.active : ''}
+              key={photo}
+              src={photo}
+              onClick={() => {
+                setCurrentPhoto(photo);
+              }}
+            />
+          );
         })}
       </div>
-      <img className={styles.mainPhoto} src={product.photos[0]} />
+      <img className={styles.mainPhoto} src={currentPhoto} />
     </FlexContainer>
   );
 }
