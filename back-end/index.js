804c27dabc5ef8fe35c0bf16279bf2dc89f34457
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-02-19 20:56:54 +0100

    refactor: switch backend to ES modules

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const productRouter = require('./src/product/product.routes').default;
-const favouriteRouter = require('./src/favourite/favourite.routes').default;
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import productRouter from './src/product/product.routes.js';
+import favouriteRouter from './src/favourite/favourite.routes.js';
+import authRouter from './src/auth/auth.routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(
   '/product-photos',
   express.static(path.join(__dirname, 'public', 'product-photos')),
 );
 app.use('/products', productRouter);
 app.use('/favourites', favouriteRouter);
+app.use('/auth', authRouter);
 
 const PORT = 3000;
 app.listen(PORT, () =>
