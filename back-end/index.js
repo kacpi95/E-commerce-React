916877af22d6865e45d71938829a499f2edf7e3d
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-10 19:55:14 +0200

    test express app

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
-import productRouter from './src/product/product.routes.js';
+import authRouter from './src/auth/auth.routes.js';
 
 const app = express();
 app.use(cors());
