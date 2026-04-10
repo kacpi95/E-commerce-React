50f46aec7deee1cc8075991186189c291db8e130
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-10 19:48:59 +0200

    test express app

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import productRouter from './src/product/product.routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API działa');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  '/product-photos',
+  express.static(path.join(__dirname, 'public', 'product-photos')),
+);
+
+app.use('/products', productRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
