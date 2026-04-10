09dfcbab3c55b4d8f17e8b5caf4588db7d63ab9c
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-10 19:47:16 +0200

    test express app

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,13 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend działa');
-});
-
-app.get('/health', (req, res) => {
-  res.json({ ok: true });
+  res.send('API działa');
 });
 
 const PORT = process.env.PORT || 3000;
