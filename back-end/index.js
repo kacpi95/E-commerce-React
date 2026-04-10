58f83f1a0cb5edf79efb27a7359d1a59deec120c
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-10 18:52:37 +0200

    install package

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
@@ -21,7 +22,7 @@ app.use('/products', productRouter);
 app.use('/favourites', favouriteRouter);
 app.use('/auth', authRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`),
 );
