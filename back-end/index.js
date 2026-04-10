ac1110714eb96361b0c1df92543305ed1e7dfb34
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-10 19:43:22 +0200

    test express app

--- back-end/index.js
+++ back-end/index.js
@@ -1,33 +1,13 @@
-import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-import productRouter from './src/product/product.routes.js';
-import favouriteRouter from './src/favourite/favourite.routes.js';
-import authRouter from './src/auth/auth.routes.js';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.get('/', (req, res) => {
+  res.send('Backend działa');
+});
 
-app.use(
-  '/product-photos',
-  express.static(path.join(__dirname, 'public', 'product-photos')),
-);
-
-app.use('/products', productRouter);
-app.use('/favourites', favouriteRouter);
-app.use('/auth', authRouter);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/health', (req, res) => {
+  res.json({ ok: true });
 });
 
 const PORT = process.env.PORT || 3000;
