f11a278ebf198446f604591be5e34d9d88ef040a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-25 21:14:50 +0200

    feat: update Product component and styles

--- front-end/src/features/products/Product/Product.jsx
+++ front-end/src/features/products/Product/Product.jsx
@@ -1,4 +1,5 @@
 import { Link, useFetcher } from 'react-router-dom';
+
 import { ENDPOINT_TO_PATH_MAPPING } from '../../../constants/api';
 import { Price } from '../../../shared/ui/Price/Price';
 import styles from './Product.module.css';
@@ -12,21 +13,31 @@ export function Product({ product }) {
       to={`/${ENDPOINT_TO_PATH_MAPPING[product.gender]}/${product.category}/${product.subcategory}/${product.id}`}
       className={styles.product}
     >
-      <img src={firstPhotoUrl} alt={product.productName} />
-      <h3>{product.productName}</h3>
-      <p>
-        <Price product={product} />
-      </p>
+      <div className={styles.imageBox}>
+        <img src={firstPhotoUrl} alt={product.productName} />
 
-      <Form
-        onClick={(e) => e.stopPropagation()}
-        method='POST'
-        action={`/add-to-favourites/${product.id}`}
-      >
-        <button type='submit' aria-label='Dodaj do ulubionych'>
-          <div className={styles.heart}></div>
-        </button>
-      </Form>
+        <Form
+          onClick={(e) => e.stopPropagation()}
+          method='POST'
+          action={`/add-to-favourites/${product.id}`}
+        >
+          <button
+            className={styles.favouriteButton}
+            type='submit'
+            aria-label='Dodaj do ulubionych'
+          >
+            <span className={styles.heart}></span>
+          </button>
+        </Form>
+      </div>
+
+      <div className={styles.productInfo}>
+        <h3>{product.productName}</h3>
+        <p className={styles.category}>{product.subcategory}</p>
+        <p className={styles.price}>
+          <Price product={product} />
+        </p>
+      </div>
     </Link>
   );
 }
