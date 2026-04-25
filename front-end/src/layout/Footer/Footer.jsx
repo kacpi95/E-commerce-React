e0fcb952fb48b1fe7c2951c5d8efd72fa3cdc472
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-25 21:23:21 +0200

    feat: update Footer component and styles

--- front-end/src/layout/Footer/Footer.jsx
+++ front-end/src/layout/Footer/Footer.jsx
@@ -2,8 +2,19 @@ import styles from './Footer.module.css';
 
 export function Footer() {
   return (
-    <div className={styles.footer}>
-      <p>Copyright 2025</p>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <h3>Clothiq®</h3>
+
+        <nav>
+          <a href='#'>Polityka prywatności</a>
+          <a href='#'>Zwroty</a>
+          <a href='#'>Kontakt</a>
+          <a href='#'>Dostawa</a>
+        </nav>
+
+        <p>© 2026 Clothiq. Wszelkie prawa zastrzeżone.</p>
+      </div>
+    </footer>
   );
 }
